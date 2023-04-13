b64a73d152adbb3d0d4ad7cb6ad84a8c0c4b9804
Aleksei Erchenko <proger@a1exxuss.ru>
2023-04-13 18:15:30 +0300

    regexp

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let regexp = /(?<=@)[A-Z0-9-]+.+.[A-Z]{2,4}$/i
+  let res = email.match(regexp);
+  if (res && res.length) {
+    return res[res.length - 1]
+  } else {
+    return null;
+  }
 }
 
 module.exports = {
