059817422792f2694d9d2becf39590817a1d2e00
Aleksei Erchenko <proger@a1exxuss.ru>
2023-04-15 00:34:24 +0300

    mine

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const WIDTH = matrix.length;
+  const HEIGHT = matrix[0].length;
+  let arrayCard = Array.from(Array(WIDTH), () => Array.from(Array(HEIGHT), () => 0))
+  let isBomb = true; // наша бомба в клетке
+  let count = 0;
+  function getNeighbors(matrix, row, col) {
+    //Создается пустой массив arr, который будет содержать соседние клетки
+    let arr = [];
+    //Запускаются два вложенных цикла for, которые проходят по всем клеткам, находящимся в квадрате 3x3 с центром в клетке (row, col). Таким образом, мы рассматриваем все клетки, которые находятся вокруг центральной клетки.
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        //Для каждой клетки, которую мы рассматриваем, проверяем, что она находится внутри границ массива matrix (т.е. ее индексы i и j должны быть больше или равны 0, меньше WIDTH и HEIGHT соответственно)
+        //Также мы проверяем, что мы не рассматриваем центральную клетку (row, col) (т.е. i и j не равны row и col соответственно).
+        if (i >= 0 && i < WIDTH && j >= 0 && j < HEIGHT && !(i === row && j === col)) {
+          arr.push(matrix[i][j]);
+        }
+      }
+    }
+    return arr;
+  }
+// пробегаемся по внешнему массиву, в котором массивы,которые формируют наше поле
+  for (let i = 0; i < WIDTH; i++) {
+     // внутренний массив [[внутр],[внутр],[внутр]]
+    for (let j = 0; j < HEIGHT; j++) {
+       // если во внутреннем массиве нашли клетку с бомбой
+      if (matrix[i][j] === isBomb) {
+          //то пометим его крестиком
+        arrayCard[i][j] = 1;
+      } else {//иначе будем искать соседей, которые говорят, сколько мин рядом окружают эту клетку вокруг
+        const neighbors = getNeighbors(matrix, i, j);
+         //фильтрует массив arr, оставляя только те элементы, которые являются истинными значениями (т.е. не false, null, 0, NaN, undefined или пустая строка). Затем он возвращает количество элементов, которые остались после фильтрации.
+        const count = neighbors.filter(cell => cell).length;
+        console.log(count)
+        arrayCard[i][j] = count;
+      }
+    }
+  }
+  return arrayCard;
+
 }
 
 module.exports = {
