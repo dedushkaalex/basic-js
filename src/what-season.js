204eaded5fee36b8f0935516afef6b7553b42c0c
Aleksei Erchenko <proger@a1exxuss.ru>
2023-04-14 18:19:27 +0300

    fix what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,31 +11,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
-  // console.debug(Object.getPrototypeOf(date))
-  if (date) {
-    const season = {
-      'spring': ['March', 'April', 'May'],
-      'summer': ['June', 'July', 'August'],
-      'autumn': ['September', 'October', 'November'],
-      'winter': ['December', 'January', 'February'],
-    };
-    const options = {
-      month: "long",
-      timeZoneName: 'short'
-    }
-    try {
-      let month = date.toLocaleDateString('en-US', options).split(',')[0];
-      for (let i in season) {
-        if (season[i].includes(month)) return i;
-      }
-    }
-    catch {
-      throw new Error('Invalid date!');
-    }
+// function getSeason(date) {
+//   // console.debug(Object.getPrototypeOf(date))
+//   if (date) {
+//     const season = {
+//       'spring': ['March', 'April', 'May'],
+//       'summer': ['June', 'July', 'August'],
+//       'autumn': ['September', 'October', 'November'],
+//       'winter': ['December', 'January', 'February'],
+//     };
+//     const options = {
+//       month: "long",
+//       timeZoneName: 'short'
+//     }
+//     try {
+//       let month = date.toLocaleDateString('en-US', options).split(',')[0];
+//       for (let i in season) {
+//         console.log(i)
+//         if (season[i].includes(month)) return i;
+//         el
+//       }
+//     }
+//     catch {
+//       throw new Error('Invalid date!');
+//     }
 
+//   }
+//   else return 'Unable to determine the time of year!';
+// }
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
   }
-  else return 'Unable to determine the time of year!';
+
+  const season = {
+    'spring': ['March', 'April', 'May'],
+    'summer': ['June', 'July', 'August'],
+    'autumn': ['September', 'October', 'November'],
+    'winter': ['December', 'January', 'February'],
+  };
+  const options = {
+    month: "long",
+    timeZoneName: 'short'
+  }
+  try {
+    let month = date.toLocaleDateString('en-US', options).split(',')[0];
+    for (let i in season) {
+      if (season[i].includes(month)) return i;
+    }
+  }
+  catch {
+    throw new Error('Invalid date!');
+  }
 }
 
 module.exports = {
